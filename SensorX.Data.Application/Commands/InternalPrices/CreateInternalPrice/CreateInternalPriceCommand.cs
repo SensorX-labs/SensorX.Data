fb1ec34857ee2f1edc00ea622d7950a071605072
=cdhai16 <chuduchai1901@gmail.com>
2026-04-18 08:06:30 +0700

    fix: sửa tên dto

--- SensorX.Data.Application/Commands/InternalPrices/CreateInternalPrice/CreateInternalPriceCommand.cs
+++ SensorX.Data.Application/Commands/InternalPrices/CreateInternalPrice/CreateInternalPriceCommand.cs
@@ -1,5 +1,5 @@
 using MediatR;
-using SensorX.Data.Application.Common.Dtos;
+using SensorX.Data.Application.Common.Dtos.Requests;
 using SensorX.Data.Application.Common.ResponseClient;
 
 namespace SensorX.Data.Application.Commands.InternalPrices.CreateInternalPrice;
@@ -9,6 +9,6 @@ public class CreateInternalPriceCommand : IRequest<Result<Guid>>
     public Guid ProductId { get; set; }
     public decimal SuggestedPrice { get; set; }
     public decimal FloorPrice { get; set; }
-    public List<PriceTierDto> PriceTiers { get; set; } = [];
+    public List<PriceTierRequest> PriceTiers { get; set; } = [];
 }
 
--- SensorX.Data.Application/Common/ResponseClient/PaginatedResult.cs
+++ SensorX.Data.Application/Common/ResponseClient/PaginatedResult.cs
@@ -7,8 +7,6 @@ public class PaginatedResult<T>
     public int PageNumber { get; set; }
     public int PageSize { get; set; }
     public int TotalPages => (TotalCount + PageSize - 1) / PageSize;
-    public bool HasPreviousPage => PageNumber > 1;
-    public bool HasNextPage => PageNumber < TotalPages;
 
     public PaginatedResult() { }
 
